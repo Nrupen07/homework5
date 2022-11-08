7acca891925e2d50dbb68668176335bf8d679c1d
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-11-08 02:15:28 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,26 +3,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Javascript</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-<style>
-    .topright {
-  position: absolute;
-  top: 8px;
-  right: 16px;
-  font-size: 18px;
-</style>
 </head>
   <body>
 
  
 <div style="background-image: url('https://html.sammy-codes.com/images/background.jpg');
 background-size: cover; height:480px; padding-top: 80px; text-align: center;">
-    <div class="topright">Click on three dots to view Javascript Functions</div>
   <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/1024px-Java_programming_language_logo.svg.png" style="height:150px; ">
   <h1 style="font-size:100px; color:white; margin:10px;">Javascript Functions</h1>
   <p style="font-size:30px; color: white;"><em>Homework 5</em></p>
   
 </div>
-...
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
