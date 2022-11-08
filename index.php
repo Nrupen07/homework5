6588d44ac08e1a6609cf09a3c0ae176b16e9fe72
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-11-08 02:13:39 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Javascript</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
+<style>
+    .topright {
+  position: absolute;
+  top: 8px;
+  right: 16px;
+  font-size: 18px;
+</style>
+</head>
   <body>
 
  
